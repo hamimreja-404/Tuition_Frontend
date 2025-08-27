ec8c508d793cea337050dd93bb3072eabc1d37a0
Hamim Reja <hamim.reja.mail@gmail.com>
2025-08-27 10:55:35 +0530

    Remove backdrop blur from Navbar component for improved styling consistency

--- src/components/Common/Navbar_.jsx
+++ src/components/Common/Navbar_.jsx
@@ -80,7 +80,7 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`sticky top-0 z-50 transition-all duration-300 shadow-md backdrop-blur-lg`}
+      className={`sticky top-0 z-50 transition-all duration-300 `}
     >
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20">
